363d368889a430c41dbf03c2faa7a907a6694306
Jovan0923 <jovanb331@deltalearns.ca>
2025-02-27 16:47:43 +0000

    Pseudocode
    Added pseudocode to the genorator.

---
+++ main.py
@@ -0,0 +1,21 @@
+# Create functions
+
+# Greet user
+
+# Ask for amount of people
+
+# Ask for names
+
+# Make invitation and loop for the amount of people
+
+# Print invitations
+
+# Ask user if they would like to add, remove, or end the genorator
+
+# Create input for adding people
+
+# Create input for removing people
+
+# Greet user goodbye
+
+
