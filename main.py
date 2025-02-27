bf9fb751c3a2dd930b72c9798566156cb47de1b7
Jovan0923 <jovanb331@deltalearns.ca>
2025-02-27 17:14:07 +0000

    Basic functions and name looping is completed.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 # Create functions
-
+# Function to add people to the list
+def add_guest(dinner_list, person):
+    dinner_list.append(person)
+    return dinner_list
+# Function to remove people from the list
+def remove_guest(dinner_list, person):
+    dinner_list.remove(person)
+    return dinner_list
 # Greet user
-
+print("Welcome to the dinner invitation generator.")
 # Ask for amount of people
-
+amount = int(input("How many people are you inviting to dinner? "))
 # Ask for names
-
+names = []
 # Make invitation and loop for the amount of people
+for i in range(amount):
+    name = input("Enter the name of the person you are inviting: ")
+    names.append(name)
+
 
 # Print invitations
 
