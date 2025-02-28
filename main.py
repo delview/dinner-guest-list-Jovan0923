982f6f862fc4bd83a732324bae3da9216c073152
Jovan0923 <jovanb331@deltalearns.ca>
2025-02-28 18:37:39 +0000

    Finished basic code

--- main.py
+++ main.py
@@ -20,13 +20,35 @@ for i in range(amount):
 
 
 # Print invitations
-
+for name in names:
+    print(f"Hello, {name}. You are invited to dinner at Slumpy Joes please let me know if you can arive.")
 # Ask user if they would like to add, remove, or end the genorator
+while True:
+    action = input("Would you like to [a]dd, [r]emove, or [e]nd the generator? ")
+    # Create input for adding people
+    if action == "a":
+        person = input("Who would you like to add? ")
+        add_guest(names, person)
+        print(f"{person} has been added to the list.")
+        for name in names:
+            print(f"Hello, {name}. You are invited to dinner at Slumpy Joes please let me know if you can arrive.")
+    # Create input for removing people
+    elif action == "r":
+        person = input("Who would you like to remove? ")
+        remove_guest(names, person)
+        print(f"{person} has been removed from the list.")
+        for name in names:
+            print(f"Hello, {name}. You are invited to dinner at Slumpy Joes please let me know if you can arrive.")
+    # Greet user goodbye
+    elif action == "e":
+        print("Goodbye.")
+        break
+    else:
+        print("Invalid input. Please try again.")
 
-# Create input for adding people
 
-# Create input for removing people
 
-# Greet user goodbye
+
+
 
 
